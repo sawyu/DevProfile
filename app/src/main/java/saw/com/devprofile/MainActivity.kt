0197c87c4d00a197492b2b71ee6bca39d83f923b
Sawyu <sawyunwetun@gmail.com>
2020-05-17 19:09:55 +0800

    Initial commit

---
+++ app/src/main/java/saw/com/devprofile/MainActivity.kt
@@ -0,0 +1,21 @@
+package saw.com.devprofile
+
+import android.graphics.BitmapFactory
+import android.media.tv.TvContract
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        logo.setImageDrawable(rounded)
+
+    }
+}
